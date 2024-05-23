abba18b29cc5f63f7022a2de0637aa7c14f32f21
TujhZreitd <egor.yakushev.96@bk.ru>
2024-05-23 14:40:52 +0300

    Тестовое задание. Утилита "поиск файлов". [#783 #497436]

--- src/main/java/ru/job4j/ArgsName.java
+++ src/main/java/ru/job4j/ArgsName.java
@@ -42,3 +42,4 @@ public class ArgsName {
         return argsName;
     }
 }
+/**/
--- src/main/java/ru/job4j/FinderFile.java
+++ src/main/java/ru/job4j/FinderFile.java
@@ -93,4 +93,5 @@ public class FinderFile {
         writeRusult(result, argsName.getValue("o"));
     }
 }
+/**/
 
